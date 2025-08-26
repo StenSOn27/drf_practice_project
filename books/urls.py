0319df22cac85865560ed73de94e2cf7bb8f2901
StenSOn27 <tarashevchik27@gmail.com>
2025-08-27 02:55:22 +0300

    Revert "Implement the crud functionality for the books service"

--- books/urls.py
+++
@@ -1,9 +0,0 @@
-﻿from rest_framework.routers import DefaultRouter
-from books.views import BookViewSet
-
-app_name = "books"
-
-router = DefaultRouter()
-router.register("", BookViewSet)
-
-urlpatterns = router.urls
