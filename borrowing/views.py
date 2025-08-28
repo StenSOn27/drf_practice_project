5ac1dd5e3b65e560b16e6ec88ca846bb9603ed69
StenSOn27 <tarashevchik27@gmail.com>
2025-08-29 02:50:56 +0300

    add the `is_active` parameter for filtering by active borrowings (not returned yet); add the `user_id` parameter for admin users, so admin can see all users

--- borrowing/views.py
+++ borrowing/views.py
@@ -9,9 +9,26 @@ class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+        queryset = Borrowing.objects.select_related('book', 'user').all()
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            is_active_bool = is_active.lower() in ["true", "1"]
+            queryset = queryset.filter(actual_return_date__isnull=is_active_bool)
+
         if user.is_staff:
-            return Borrowing.objects.select_related('book', 'user').all()
-        return Borrowing.objects.select_related('book', 'user').filter(user=user)
+            user_id_param = self.request.query_params.get("user_id")
+            if user_id_param:
+                try:
+                    user_id = int(user_id_param)
+                    queryset = queryset.filter(user_id=user_id)
+                except ValueError:
+                    pass
+
+        return queryset
 
 
 class BorrowingCreateView(generics.CreateAPIView):
