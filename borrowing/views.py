93848f6f70d4dfca5c2a5deb86ab6da28e3e653f
StenSOn27 <tarashevchik27@gmail.com>
2025-08-28 23:29:09 +0300

    implement create view

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Borrowing
-from .serializers import BorrowingReadSerializer
+from .serializers import BorrowingCreateSerializer, BorrowingReadSerializer
 
 
 class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Borrowing.objects.select_related('book', 'user').all()
     serializer_class = BorrowingReadSerializer
+
+
+class BorrowingCreateView(generics.CreateAPIView):
+    serializer_class = BorrowingCreateSerializer
