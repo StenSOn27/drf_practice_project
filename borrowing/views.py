96e1a71d5abdd31c24be40f640260edd46a4d420
StenSOn27 <tarashevchik27@gmail.com>
2025-08-29 01:52:45 +0300

    change views: each non-admin can see only their own borrowings

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,12 +1,19 @@
 from rest_framework import viewsets, generics
 from .models import Borrowing
 from .serializers import BorrowingCreateSerializer, BorrowingReadSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Borrowing.objects.select_related('book', 'user').all()
     serializer_class = BorrowingReadSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Borrowing.objects.select_related('book', 'user').all()
+        return Borrowing.objects.select_related('book', 'user').filter(user=user)
 
 
 class BorrowingCreateView(generics.CreateAPIView):
     serializer_class = BorrowingCreateSerializer
+    permission_classes = [IsAuthenticated]
