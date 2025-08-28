e53f582e1c2897e28da91e8ba6e6901c0fbda8ea
StenSOn27 <tarashevchik27@gmail.com>
2025-08-28 23:29:54 +0300

    change routes logic

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,9 +1,13 @@
-﻿from rest_framework.routers import DefaultRouter
-from borrowing.views import BorrowingViewSet
+﻿from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from borrowing.views import BorrowingViewSet, BorrowingCreateView
 
 app_name = "borrowings"
 
 router = DefaultRouter()
-router.register("", BorrowingViewSet)
+router.register("", BorrowingViewSet, basename="borrowings")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("create/", BorrowingCreateView.as_view(), name="borrowing-create"),
+    path("", include(router.urls)),
+]
