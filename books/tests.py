2401e2b18e7ee06767cc00e2d1fe46ef72077bd7
StenSOn27 <tarashevchik27@gmail.com>
2025-09-01 19:51:03 +0300

    implement book model test

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Book, Cover
 
-# Create your tests here.
+class BookModelTests(TestCase):
+    def test_create_book(self):
+        book = Book.objects.create(
+            title="Test Title",
+            author="Test Author",
+            cover=Cover.HARD.value,
+            inventory=5,
+            daily_fee="1.50",
+        )
+        self.assertEqual(book.title, "Test Title")
+        self.assertEqual(book.author, "Test Author")
+        self.assertEqual(book.cover, "HARD")
+        self.assertEqual(book.inventory, 5)
+        self.assertEqual(str(book.daily_fee), "1.50")
+        self.assertEqual(str(book), "Test Title")
